97ec2aa7ee04549ae5ac52ce28eeeac39981840d
dieudonne-mbuyi <dieudonnem484@gmail.com>
2025-11-12 21:38:42 +0200

    initial commit de Golden salon

---
+++ js/app.js
@@ -0,0 +1,14 @@
+[
+    {
+        "type": "image",
+        "src": "images/nouvelle_coiffure.jpg",
+        "title": "Nouvelle coiffure tendance",
+        "text": "Venez découvrir nos nouvelles coupes et tresses modernes."
+    },
+    {
+        "type": "video",
+        "src": "videos/presentation.mp4",
+        "title": "Vidéo de présentation du salon",
+        "text": "Une courte visite guidée de GOLDEN SALON."
+    }
+]
